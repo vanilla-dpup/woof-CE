14e82da95bc1661b88144203704a316219a8e234
Dima Krasner <dima@dimakrasner.com>
2023-06-09 11:08:52 +0300

    restore package templates

--- woof-code/rootfs-petbuilds/transmission/pinstall.sh
+++ woof-code/rootfs-petbuilds/transmission/pinstall.sh
@@ -1,3 +1,5 @@
 echo '#!/bin/sh
 exec transmission-gtk "$@"' > usr/local/bin/defaulttorrent
 chmod 755 usr/local/bin/defaulttorrent
+
+chroot . /usr/sbin/setup-spot transmission-gtk=true
--- woof-code/rootfs-petbuilds/weechat/pinstall.sh
+++ woof-code/rootfs-petbuilds/weechat/pinstall.sh
@@ -1,3 +1,8 @@
 echo '#!/bin/ash
 exec weechat-shell' > usr/local/bin/defaultchat
 chmod 755 usr/local/bin/defaultchat
+
+chroot . /usr/sbin/setup-spot weechat=true
+
+chroot . run-as-spot weechat-headless -r "/server add libera irc.libera.chat/6697 -autoconnect -ssl;/set irc.server.libera.autojoin #puppylinux;/quit"
+rm -f usr/bin/weechat-headless
