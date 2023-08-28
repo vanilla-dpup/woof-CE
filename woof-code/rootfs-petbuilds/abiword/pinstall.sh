9c4a42a03e7eb8398c1eae5dd7eca70e39cbbd74
Dima Krasner <dima@dimakrasner.com>
2023-08-28 19:16:30 +0300

    drop abiword and gnumeric petbuilds

--- woof-code/rootfs-petbuilds/abiword/pinstall.sh
+++
@@ -1,3 +0,0 @@
-echo '#!/bin/sh
-exec abiword "$@"' > usr/local/bin/defaultwordprocessor
-chmod 755 usr/local/bin/defaultwordprocessor
--- woof-code/rootfs-petbuilds/gnumeric/pinstall.sh
+++
@@ -1,3 +0,0 @@
-echo '#!/bin/sh
-exec gnumeric "$@"' > usr/local/bin/defaultspreadsheet
-chmod 755 usr/local/bin/defaultspreadsheet
