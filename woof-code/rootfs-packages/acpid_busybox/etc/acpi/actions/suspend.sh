33b276324cd5e7e2229ecfa3fc21978547653821
Dima Krasner <dima@dimakrasner.com>
2024-04-28 13:59:50 +0300

    disable the internal display without suspend on lid close with connected monitor

--- woof-code/rootfs-packages/acpid_busybox/etc/acpi/actions/suspend.sh
+++ woof-code/rootfs-packages/acpid_busybox/etc/acpi/actions/suspend.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # suspend.sh 28sep09 by shinobar
 # 12feb10 pass poweroff
 # 23apr12 fix was not suspend from acpi_poweroff.sh
@@ -6,6 +6,40 @@
 #20140629 shinobar: ACPI_CONFIG
 ACPI_CONFIG=/etc/acpi/acpi.conf
 [ -s "$ACPI_CONFIG" ] && . "$ACPI_CONFIG"
+
+case "$(awk '{print $2}' /proc/acpi/button/lid/LID0/state)" in
+# if the lid is closed:
+#  if an external monitor is connected and enabled:
+#   turn off the internal display
+#  otherwise
+#   suspend and let the screen locker take care of turning off the internal display while locked
+closed)
+  if [ -n "`comm -12 <(grep -l '^connected$' /sys/class/drm/*/status | cut -f 5 -d / | sort) <(grep -l '^enabled$' /sys/class/drm/*/enabled | cut -f 5 -d / | sort) | grep -Fv -e eDP -e LVDS`" ]; then
+    if [ -n "$WAYLAND_DISPLAY" ]; then
+      wlr-randr --output eDP-1 --off || wlr-randr --output eDP1 --off || wlr-randr --output LVDS-1 --off || wlr-randr --output LVDS1 --off
+    elif [ -n "$DISPLAY" ]; then
+      xrandr --output eDP-1 --off || xrandr --output eDP1 --off || xrandr --output LVDS-1 --off || xrandr --output LVDS1 --off
+    fi
+    touch /tmp/.lid-closed
+    DISABLE_SUSPEND=y
+  fi
+  ;;
+# if the lid is opened:
+#  turn on the previously disabled internal display
+# (we must do this because we don't know if the laptop was supended while connected to an external monitor)
+open)
+  if [ -f /tmp/.lid-closed ]; then
+    if [ -n "$WAYLAND_DISPLAY" ]; then
+      wlr-randr --output eDP-1 --on || wlr-randr --output eDP1 --on || wlr-randr --output LVDS-1 --on || wlr-randr --output LVDS1 --on
+    elif [ -n "$DISPLAY" ]; then
+      xrandr --output eDP-1 --auto || xrandr --output eDP1 --auto || xrandr --output LVDS-1 --auto || xrandr --output LVDS1 --auto
+    fi
+    rm -f /tmp/.lid-closed
+  fi
+  DISABLE_SUSPEND=y
+  ;;
+esac
+
 case "$DISABLE_SUSPEND" in
 y*|Y*|true|True|TRUE|1) exit;;
 esac
