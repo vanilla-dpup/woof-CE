fde33949fe7c5d54bf24b325ff6dcbff0999062f
Dima Krasner <dima@dimakrasner.com>
2024-06-13 17:12:40 +0000

    remove more remains of X support

--- woof-code/rootfs-petbuilds/dwl/pinstall.sh
+++ woof-code/rootfs-petbuilds/dwl/pinstall.sh
@@ -3,24 +3,11 @@ cat << EOF >> etc/environment
 XDG_CURRENT_DESKTOP=wlroots
 EOF
 
-if [ -f usr/bin/jwm ]; then
-	cat << EOF >> etc/environment
-DWL_BORDER_COLOR=#000000
-DWL_FOCUS_COLOR=#000000
-DWL_ROOT_COLOR=#000000
-GDK_BACKEND=x11
-QT_QPA_PLATFORM=xcb
-SDL_VIDEODRIVER=x11
-EOF
-else
-	cat << EOF >> etc/environment
+cat << EOF >> etc/environment
 DWL_BORDER_COLOR=#000000
 DWL_FOCUS_COLOR=#FF00FF
 DWL_URGENT_COLOR=#00FFFF
 DWL_ROOT_COLOR=#221111
 EOF
-fi
-
-[ -f root/.jwm/jwmrc-personal ] && sed -i 's~^<Key key="Print">exec:defaultscreenshot</Key>$~&\n<Key mask="S" key="Print">exec:slurp-screenshot</Key>~' root/.jwm/jwmrc-personal
 
 echo dwl > etc/windowmanager
