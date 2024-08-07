0be70f7ff8d5b139241c7a8317eefb547d65f92b
Dima Krasner <dima@dimakrasner.com>
2024-08-07 12:00:38 +0300

    unify {mtpaint,slurp}-screenshot

--- woof-code/rootfs-petbuilds/mtpaint/pinstall.sh
+++ woof-code/rootfs-petbuilds/mtpaint/pinstall.sh
@@ -3,7 +3,7 @@ exec mtpaint "$@"' > usr/local/bin/defaultimageeditor
 chmod 755 usr/local/bin/defaultimageeditor
 
 echo '#!/bin/sh
-exec mtpaint-screenshot "$@"' > usr/local/bin/defaultscreenshot
+exec mtpaint-screenshot full "$@"' > usr/local/bin/defaultscreenshot
 chmod 755 usr/local/bin/defaultscreenshot
 
 if [ ! -e usr/local/apps/ROX-Filer/AppRun ]; then
