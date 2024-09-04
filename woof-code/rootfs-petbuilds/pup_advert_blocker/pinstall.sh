205270cd35374a2d45a46368c6b65764e2298f9e
Dima Krasner <dima@dimakrasner.com>
2024-09-04 15:49:10 +0300

    re-enable ad blocking by default, disabled by 5dd8fed

--- woof-code/rootfs-petbuilds/pup_advert_blocker/pinstall.sh
+++ woof-code/rootfs-petbuilds/pup_advert_blocker/pinstall.sh
@@ -2,5 +2,5 @@ sed -i 's/ dns$/ adlist dns/g' etc/nsswitch.conf
 
 rm -f etc/resolv.conf
 cp -f /etc/resolv.conf etc/resolv.conf
-chroot . /usr/sbin/pup-advert-blocker start
+chroot . /usr/sbin/pup-advert-blocker update
 rm -f etc/resolv.conf
